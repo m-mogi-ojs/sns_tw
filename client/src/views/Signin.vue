3618fb6ac8666b1297f0cf2b2e18897a6517ef0f
m-mogi-ojs <m.mogi.ojs@gmail.com>
2019-09-19 14:37:54 +0900

    add signup and signin.

---
+++ client/src/views/Signin.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="signin container">
+    <h1 class="label">ログイン</h1>
+    <b-field horizontal grouped label="ユーザー名" label-position="inside">
+      <b-input type="text" placeholder="Username" v-model="username"></b-input>
+    </b-field>
+    <b-field horizontal label="パスワード">
+      <b-input type="password" placeholder="Password" v-model="password"></b-input>
+    </b-field>
+    <button class="button is-info">ログインする</button>
+    <p>会員登録がまだお済みでない方は<router-link to="/signup">こちらから会員登録</router-link></p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'signin',
+  data () {
+    return {
+      username: '',
+      password: ''
+    }
+  },
+  methods: {}
+}
+</script>
+
+<style lang="scss" scoped>
+p {
+  padding-top: 1rem;
+}
+</style>
---
+++ client/src/views/Signup.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="signup container">
+    <h1 class="label">会員登録</h1>
+    <b-field horizontal grouped label="ユーザー名" label-position="inside">
+      <b-input type="text" placeholder="Username" v-model="username"></b-input>
+    </b-field>
+    <b-field horizontal label="パスワード">
+      <b-input type="password" placeholder="Password" v-model="password"></b-input>
+    </b-field>
+    <button class="button is-info">登録する</button>
+    <p>会員登録済みの方は<router-link to="/signin">こちらからログイン</router-link></p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'signup',
+  data () {
+    return {
+      username: '',
+      password: ''
+    }
+  },
+  methods: {}
+}
+</script>
+
+<style lang="scss" scoped>
+p {
+  padding-top: 1rem;
+}
+</style>
